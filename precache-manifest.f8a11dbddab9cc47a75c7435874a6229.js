601cebacfbad69c51882803fa98ce5b3b1fc0cd7
fanwentao <fanwentaocumt@163.com>
2020-04-15 14:04:47 +0800

    Updates

---
+++ precache-manifest.f8a11dbddab9cc47a75c7435874a6229.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f83585d6ddceb71a55913ada1479b5",
+    "url": "/portal/index.html"
+  },
+  {
+    "revision": "b26eea19458a238b1c5d",
+    "url": "/portal/static/css/3.8455d1a0.chunk.css"
+  },
+  {
+    "revision": "f8a55f42d68341ac8607",
+    "url": "/portal/static/css/4.dc27931e.chunk.css"
+  },
+  {
+    "revision": "39b2b09e334d8bfd920b",
+    "url": "/portal/static/css/5.cecede16.chunk.css"
+  },
+  {
+    "revision": "ead88746a7b10c3c5651",
+    "url": "/portal/static/css/main.6839efc3.chunk.css"
+  },
+  {
+    "revision": "70ac36c7b4181ead40e2",
+    "url": "/portal/static/js/2.b20575ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portal/static/js/2.b20575ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b26eea19458a238b1c5d",
+    "url": "/portal/static/js/3.7031c2c3.chunk.js"
+  },
+  {
+    "revision": "f8a55f42d68341ac8607",
+    "url": "/portal/static/js/4.f663e5f6.chunk.js"
+  },
+  {
+    "revision": "39b2b09e334d8bfd920b",
+    "url": "/portal/static/js/5.02b0939e.chunk.js"
+  },
+  {
+    "revision": "ead88746a7b10c3c5651",
+    "url": "/portal/static/js/main.b38c70dc.chunk.js"
+  },
+  {
+    "revision": "769a98b65715497112a1",
+    "url": "/portal/static/js/runtime-main.f3430ea3.js"
+  },
+  {
+    "revision": "adcddc79111e41f9583ade70546d2f20",
+    "url": "/portal/static/media/search.adcddc79.svg"
+  }
+]);
