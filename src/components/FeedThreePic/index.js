fe5fe6017f41604a8ee2414c45aaf19f81c3f238
fanwentao <fanwentaocumt@163.com>
2020-04-17 17:10:15 +0800

    add author head

--- src/components/FeedThreePic/index.js
+++ src/components/FeedThreePic/index.js
@@ -2,15 +2,27 @@ import React from 'react';
 import SmartLink from '../SmartLink';
 
 /**
- * @param {string} data 三图模板对象
+ * @param {object} data 大图模板对象
  * @param {string} data.id 信息流id
- * @param {string} data.title 信息流标题
- * @param {string} data.info 信息流额外信息
- * @param {array} data.pics 图片数组，必须三张图
+ * @param {string} data.title 标题
+ * @param {string} data.publishTime 发布时间
+ * @param {number} data.commentNum 评论数
+ * @param {string} data.feedType 信息流模板类型
+ * @param {array} data.pics 图片数组
  * @param {string} data.adLink 广告链接
  * @param {boolean} data.isAd 是不是广告
  * @param {boolean} data.isTop 是不是置顶
  * @param {boolean} data.isHot 是不是热门
+ * @param {boolean} data.showHead 是否显示头像
+ * @param {object} author 作者对象
+ * @param {string} author.id 作者id
+ * @param {string} author.name 作者名称
+ * @param {string} author.avatar 作者头像
+ * @param {string} author.label 作者标签
+ * @param {boolean} author.vip 作者是否是vip
+ * @param {boolean} author.friend 是否关注了作者
+ * @param {function} onFriend 关注或者取关 (isFriend) => {}
+ * @param {function} onRemove 移除广告
 **/
 class FeedThreePic extends React.Component {
   constructor(props) {
@@ -18,33 +30,58 @@ class FeedThreePic extends React.Component {
     this.state = {}
   }
 
-  handleRemove = e => {
+  preventClick = e => {
     e.stopPropagation();
     e.preventDefault();
+  }
+
+  handleRemove = () => {
     this.props.onRemove && this.props.onRemove();
+  }
+
+  handleFriend = () => {
+    this.props.onFriend && this.props.onFriend(this.props.data.author.friend);
   }
 
   render() {
     const { data } = this.props;
-    const pics = data.pics;
-    const { isAd, isTop, isHot, adLink } = data;
+    const { title, publishTime, commentNum, pics, author, isAd, isTop, isHot, adLink, showHead } = data;
+    const { name, avatar, label, vip, friend } = author;
     const linkUrl = {
-      href: isAd ? adLink : '',
-      to: isAd ? '' : `/p/${data.id}`
+      href: isAd && adLink,
+      to: !isAd && `/p/${data.id}`
     };
 
     return (
       <SmartLink className="feed feed-threepic" {...linkUrl}>
-        <div className="feed-tit line3">{data.title}</div>
+        {showHead && <div className="feed-hd" onClick={this.preventClick}>
+          <div className="author-avatar">
+            <img src={avatar} alt=""/>
+            {vip && <span className="vip" />}
+          </div>
+          <div className="author-info">
+            <div className="name">{name}</div>
+            <div className="label">{label}</div>
+          </div>
+          <div className="author-friend">
+            <span
+              className={friend ? 'friend' : 'unfriend'}
+              onClick={this.handleFriend}
+            >{friend ? '已关注' : '关注'}</span>
+          </div>
+        </div>}
+        <div className="feed-tit line3">{title}</div>
         {pics.length > 0 && <div className="feed-pic">
           {pics.map((pic, i) => <img key={i} src={pic} alt=""/>)}
         </div>}
-        <div className="feed-info">
+        <div className="feed-info" onClick={this.preventClick}>
           <div className="info-l">
             {isAd && <span className="pretag">广告</span>}
             {isTop && <span className="pretag red">置顶</span>}
-            {isHot && <span className="pretag red">热</span>}
-            {data.info}
+            {isHot && <span className="pretag red">热门</span>}
+            {!showHead && <span>{name}</span>}
+            {!isAd && <span>评论 {commentNum}</span>}
+            <span>{publishTime}</span>
           </div>
           <div className="info-r">
             {isAd && <span className="pretag close" onClick={this.handleRemove} />}
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -12,6 +12,7 @@ const placePic = {
   threePic: 'https://placekitten.com/170/120',
   bigPic: 'https://placekitten.com/630/315',
   videoPic: 'https://placekitten.com/600/310',
+  avatarPic: 'https://placekitten.com/100/100'
 };
 
 const feedComponents = {
@@ -21,91 +22,171 @@ const feedComponents = {
   feedvideo: FeedVideo
 }
 
-const feedList = [
+const mockFeedList = [
   {
     id: '6815573013170225671',
     title: '习近平：团结合作是国际社会战胜疫情最有力武器',
-    info: '中央新闻网 评论 8993 刚刚',
-    layout: 'bigpic',
+    publishTime: '刚刚',
+    commentNum: 1,
+    feedType: 'bigpic',
+    pics: [],
+    author: {
+      name: '中央新闻网',
+      avatar: placePic.avatarPic,
+      label: '中央新闻网官方账号',
+      vip: true
+    },
     isTop: true,
-    pics: []
   },
   {
     id: '6815586582116958734',
     title: '人民日报和音：特殊时期最宝贵的人权保护',
-    info: '人民日报 评论 2466 1分钟前',
-    layout: 'bigpic',
+    publishTime: '1分钟前',
+    commentNum: 14,
+    feedType: 'bigpic',
+    pics: [],
+    author: {
+      name: '人民日报',
+      avatar: placePic.avatarPic,
+      label: '人民日报官方账号',
+      vip: true
+    },
     isTop: true,
-    pics: []
   },
   {
     id: '6815612142201340430',
     title: '中国抗疫图鉴（完整版）',
-    info: '人民日报 评论 785 4分钟前',
-    layout: 'threepic',
+    publishTime: '3分钟前',
+    commentNum: 70,
+    feedType: 'threepic',
+    pics: [placePic.threePic, placePic.threePic, placePic.threePic],
+    author: {
+      name: '人民日报',
+      avatar: placePic.avatarPic,
+      label: '人民日报官方账号',
+      vip: true
+    },
     isHot: true,
-    pics: [placePic.threePic, placePic.threePic, placePic.threePic]
   },
   {
     id: '6815583319925719560',
     title: '林郑月娥：立法会部分议员恶意“拉布”铁证如山不容抵赖',
-    info: '新华网客户端 评论 0 5分钟前',
-    layout: 'bigpic',
-    pics: []
+    publishTime: '3分钟前',
+    commentNum: 1,
+    feedType: 'bigpic',
+    pics: [],
+    author: {
+      name: '新华网客户端',
+      avatar: placePic.avatarPic,
+      label: '新华网官方账号',
+      vip: true
+    },
   },
   {
     id: '6805823214321664515',
     title: '伊拉克战争，为何萨达姆放着800架战机不用？这是他真正聪明之处',
-    info: '央视网新闻 评论 2 8分钟前',
-    layout: 'smallpic',
-    pics: [placePic.smallPic]
+    publishTime: '5分钟前',
+    commentNum: 2,
+    feedType: 'smallpic',
+    pics: [placePic.smallPic],
+    author: {
+      name: '央视网新闻',
+      avatar: placePic.avatarPic,
+      label: '央视网新闻官方账号',
+      vip: true
+    },
+    showHead: true,
   },
   {
     id: '6815362200710939149',
     title: '26家！美国紧急授权KN95口罩，这些上市公司拿到出口通行证',
-    info: '中国经济网 评论 25 6分钟前',
-    layout: 'threepic',
-    pics: [placePic.threePic, placePic.threePic, placePic.threePic]
+    publishTime: '6分钟前',
+    commentNum: 45,
+    feedType: 'threepic',
+    pics: [placePic.threePic, placePic.threePic, placePic.threePic],
+    author: {
+      name: '中国经济网',
+      avatar: placePic.avatarPic,
+      label: '中国经济网官方账号',
+      vip: true
+    },
   },
   {
     id: '6815537455467332103',
     title: '拿回一大袋葱，好奇的各种闻舔咬。',
-    info: '秀外慧中2008 评论 0 18分钟前',
+    publishTime: '18分钟前',
+    commentNum: 233,
+    feedType: 'feedvideo',
     duration: '01:29',
-    layout: 'feedvideo',
-    pics: [placePic.videoPic]
+    pics: [placePic.videoPic],
+    author: {
+      name: '秀外慧中2008',
+      avatar: placePic.avatarPic,
+      label: '优质生活信息创作者'
+    },
+    showHead: true,
   },
   {
     id: '6780199724038225934',
     title: '基于React+Antd打造的后台管理系统示例，不想研究一下吗？',
-    info: '涛涛哥哥 1天前',
-    layout: 'bigpic',
-    isAd: true,
+    publishTime: '1天前',
+    commentNum: 0,
+    feedType: 'bigpic',
     adLink: 'https://fwtcumt.github.io/system-antd3.0',
-    pics: [placePic.bigPic]
+    pics: [placePic.bigPic],
+    author: {
+      name: '涛涛哥哥',
+      avatar: placePic.avatarPic,
+      label: '优秀技术领域创作者',
+      vip: true
+    },
+    isAd: true,
   },
   {
     id: '6815902084869653004',
     title: '奥巴马宣布支持美民主党总统竞选人拜登',
-    info: '中国新闻网 评论 0 2天前',
-    layout: 'bigpic',
-    pics: [placePic.bigPic]
+    publishTime: '2天前',
+    commentNum: 0,
+    feedType: 'bigpic',
+    pics: [placePic.bigPic],
+    author: {
+      name: '中国新闻网',
+      avatar: placePic.avatarPic,
+      label: '中国新闻网官方账号',
+      vip: true,
+      friend: true
+    },
+    showHead: true,
   },
   {
     id: '6813595080142619144',
     title: '摸金校尉、尸香魔芋、黑驴蹄子...《鬼吹灯》这些“专业词汇”来自哪里？',
-    info: '三联生活周刊 评论 25 04-15 23:58',
-    layout: 'threepic',
-    pics: [placePic.threePic, placePic.threePic, placePic.threePic]
+    publishTime: '04-12 22:50',
+    commentNum: 10,
+    feedType: 'threepic',
+    pics: [placePic.threePic, placePic.threePic, placePic.threePic],
+    author: {
+      name: '三联生活周刊',
+      avatar: placePic.avatarPic,
+      label: '优质自媒体',
+      vip: true
+    },
   },
   {
     id: '6814744443728462350',
     title: 'JavaScript中文网，为了你及时的武装自己',
-    info: '鸿儿哥哥 04-15 22:58',
-    layout: 'bigpic',
-    isAd: true,
+    publishTime: '04-15 22:58',
+    commentNum: 0,
+    feedType: 'bigpic',
+    pics: [placePic.bigPic],
+    author: {
+      name: '鸿儿哥哥',
+      avatar: placePic.avatarPic,
+      label: '专业打广告'
+    },
     adLink: 'https://www.javascriptcn.com/',
-    pics: [placePic.bigPic]
+    isAd: true,
+    showHead: true,
   },
 ];
 
@@ -113,7 +194,8 @@ class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      navType: 1
+      navType: 1,
+      feedList: mockFeedList
     }
   }
 
@@ -121,18 +203,39 @@ class Home extends React.Component {
   handleChangeNav = (type) => {
     this.setState({ navType: type });
   }
+
+  // 关注作者
+  handleFeedFriend = (idx, isFriend) => {
+    const { feedList } = this.state;
+    feedList[idx].author.friend = !isFriend;
+    this.setState({ feedList });
+  }
+
+  // 移除广告
+  handleFeedRemove = (idx) => {
+    const { feedList } = this.state;
+    feedList.splice(idx, 1);
+    this.setState({ feedList });
+  }
    
   render() {
-    const { navType } = this.state;
+    const { navType, feedList } = this.state;
 
     return (
       <div className="page-root page-home">
         <Header />
         <Navbar type={navType} onChangeNav={this.handleChangeNav} />
         <div className="feedlist">
-          {feedList.map(item => {
-            const FeedItem = feedComponents[item.layout];
-            return <FeedItem key={item.id} data={item} />;
+          {feedList.map((item, idx) => {
+            const FeedItem = feedComponents[item.feedType];
+            return (
+              <FeedItem
+                key={item.id}
+                data={item}
+                onFriend={(isFriend) => this.handleFeedFriend(idx, isFriend)}
+                onRemove={() => this.handleFeedRemove(idx)}
+              />
+            );
           })}
           <div className="feed-none">————————— 到底了 —————————</div>
         </div>
